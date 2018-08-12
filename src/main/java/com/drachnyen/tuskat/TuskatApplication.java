80205ee9ba0f430d90c0ea68d55ba07c5999dad0
samurus <conann233@web.de>
2018-08-12 23:01:04 +0200

    init

---
+++ src/main/java/com/drachnyen/tuskat/TuskatApplication.java
@@ -0,0 +1,12 @@
+package com.drachnyen.tuskat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TuskatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TuskatApplication.class, args);
+	}
+}
---
+++ src/test/java/com/drachnyen/tuskat/TuskatApplicationTests.java
@@ -0,0 +1,16 @@
+package com.drachnyen.tuskat;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TuskatApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
